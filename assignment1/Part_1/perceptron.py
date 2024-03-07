3ab10fb647089d8ddbd4486e2ff3b4181cc5c300
Rock3Yu <rockyu787@gmail.com>
2024-03-07 20:59:10 +0800

    begin with a few lines of changes

--- assignment1/Part_1/perceptron.py
+++ assignment1/Part_1/perceptron.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+
 class Perceptron(object):
 
-     def __init__(self, n_inputs, max_epochs=___, learning_rate=___):
+    def __init__(self, n_inputs, max_epochs=None, learning_rate=None):
         """
         Initializes the perceptron object.
         - n_inputs: Number of inputs.
@@ -10,11 +11,11 @@ class Perceptron(object):
         - learning_rate: Magnitude of weight changes at each training cycle.
         - weights: Initialize weights (including bias).
         """
-        self.n_inputs = ___  # Fill in: Initialize number of inputs
-        self.max_epochs = ___  # Fill in: Initialize maximum number of epochs
-        self.learning_rate = ___  # Fill in: Initialize learning rate
-        self.weights = ___  # Fill in: Initialize weights with zeros 
-        
+        self.n_inputs = None                # Fill in: Initialize number of inputs
+        self.max_epochs = max_epochs        # Fill in: Initialize maximum number of epochs
+        self.learning_rate = learning_rate  # Fill in: Initialize learning rate
+        self.weights = None                 # Fill in: Initialize weights with zeros
+
     def forward(self, input_vec):
         """
         Predicts label from input.
@@ -23,7 +24,8 @@ class Perceptron(object):
         Returns:
             int: Predicted label (1 or -1) or Predicted lables.
         """
-        
+        pass
+
     def train(self, training_inputs, labels):
         """
         Trains the perceptron.
@@ -32,7 +34,7 @@ class Perceptron(object):
             labels (np.ndarray): Array of expected output values for the corresponding point in training_inputs.
         """
         # we need max_epochs to train our model
-        for _ in range(self.max_epochs): 
+        for _ in range(self.max_epochs):
             """
                 What we should do in one epoch ? 
                 you are required to write code for 
@@ -42,4 +44,5 @@ class Perceptron(object):
                 4.Using gradient descent method to update parameters(not Stochastic gradient descent!,
                 please follow the algorithm procedure in "perceptron_tutorial.pdf".)
             """
-        
+            pass
+        pass
