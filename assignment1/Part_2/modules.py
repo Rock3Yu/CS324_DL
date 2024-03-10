3fe33a8710a4c0f4d04de2bfff8fbe62016edac8
Rock3Yu <rockyu787@gmail.com>
2024-03-10 12:01:09 +0800

    done mlp modules, needs check

--- assignment1/Part_2/modules.py
+++ assignment1/Part_2/modules.py
@@ -1,78 +1,128 @@
 import numpy as np
 
+
 class Linear(object):
-    def __init__(self, in_features, out_features):
+    def __init__(self, in_features, out_features, learning_rate=1e-2):
         """
         Initializes a linear (fully connected) layer. 
-        TODO: Initialize weights and biases.
         - Weights should be initialized to small random values (e.g., using a normal distribution).
         - Biases should be initialized to zeros.
         Formula: output = x * weight + bias
+        Args:
+            in_features: number of input
+            out_features: number of output
         """
-        # Initialize weights and biases with the correct shapes.
-        self.params = {'weight': None, 'bias': None}
-        self.grads = {'weight': None, 'bias': None}
+        self.x = None
+        self.in_features = in_features
+        self.out_features = out_features
+        self.lr = learning_rate
+        self.params = {'weight': np.random.randn(in_features, out_features) * 1e-2,
+                       'bias': np.zeros((out_features,))}
+        self.grads = {'weight': np.random.randn(in_features, out_features) * 1e-2,
+                      'bias': np.zeros((out_features,))}
 
     def forward(self, x):
         """
         Performs the forward pass using the formula: output = xW + b
-        TODO: Implement the forward pass.
+        Args:
+            x: input data
         """
-        return None
+        self.x = x
+        return np.dot(x, self.params['weight']) + self.params['bias']
 
     def backward(self, dout):
         """
-        Backward pass to calculate gradients of loss w.r.t. weights and inputs.
-        TODO: Implement the backward pass.
+        Backward propagation to calculate gradients of loss w.r.t. weights and inputs.
+        Args:
+            dout: Upstream derivative
+        done: Implement the backward pass.
         """
-        return None
+        dx = np.dot(dout, self.params['weight'].T)
+        self.grads['weight'] = np.dot(self.x.T, dout)  # dw
+        self.grads['bias'] = np.sum(dout, axis=0)  # db
+        return dx
+
+    def update(self):
+        self.params['weight'] -= self.lr * self.grads['weight']
+        self.params['bias'] -= self.lr * self.grads['bias']
+
+    def __call__(self, x):
+        return self.forward(x)
+
 
 class ReLU(object):
+    def __init__(self):
+        self.x = None
+
     def forward(self, x):
         """
         Applies the ReLU activation function element-wise to the input.
         Formula: output = max(0, x)
-        TODO: Implement the forward pass.
+        Args:
+            x: input data
         """
-        return None
+        self.x = x
+        return np.maximum(0, x)
 
     def backward(self, dout):
         """
         Computes the gradient of the ReLU function.
-        TODO: Implement the backward pass.
         Hint: Gradient is 1 for x > 0, otherwise 0.
+        Args:
+            dout: Upstream derivative
         """
-        return None
+        dx = dout * (self.x > 0)
+        return dx
+
+    def __call__(self, x):
+        return self.forward(x)
+
 
 class SoftMax(object):
+
+    def __init__(self):
+        self.x = None
+
     def forward(self, x):
         """
         Applies the softmax function to the input to obtain output probabilities.
         Formula: softmax(x_i) = exp(x_i) / sum(exp(x_j)) for all j
-        TODO: Implement the forward pass using the Max Trick for numerical stability.
+        Args:
+            x: input data
         """
-        return None
+        self.x = x
+        return np.exp(x) / np.sum(np.exp(x), axis=0)
 
     def backward(self, dout):
         """
-        The backward pass for softmax is often directly integrated with CrossEntropy for simplicity.
-        TODO: Keep this in mind when implementing CrossEntropy's backward method.
+        The backward propagation for softmax is often directly integrated with CrossEntropy for simplicity.
+        Args:
+            dout: Upstream derivative
         """
-        return None
+        return dout
+
+    def __call__(self, x):
+        return self.forward(x)
+
 
 class CrossEntropy(object):
     def forward(self, x, y):
         """
         Computes the CrossEntropy loss between predictions and true labels.
         Formula: L = -sum(y_i * log(p_i)), where p is the softmax probability of the correct class y.
-        TODO: Implement the forward pass.
+        Args:
+            x: input data
+            y: true labels
         """
-        return None
+        return -np.sum(y * np.log(x))
 
     def backward(self, x, y):
         """
         Computes the gradient of CrossEntropy loss with respect to the input.
-        TODO: Implement the backward pass.
         Hint: For softmax output followed by cross-entropy loss, the gradient simplifies to: p - y.
         """
-        return None
+        return x - y
+        # return (x - y) / len(x)
+
+    def __call__(self, x, y):
+        return self.forward(x, y)
